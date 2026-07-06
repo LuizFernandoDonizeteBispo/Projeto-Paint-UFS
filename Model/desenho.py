87f9940072ea8bad830d317029da402d7b6dd4a5
Andrey Ariel Silva de Almeida <andreyarielsilvadealmeida@gmail.com>
2026-07-06 20:15:30 -0300

    Update desenho.py
    armazem de figuras

--- Model/desenho.py
+++ Model/desenho.py
@@ -1,55 +1,10 @@
-from View.seletor_cor import SeletorCor
-from Model import *
+from figuras import Figura
 
-class Desenho:
-
-    def __init__(self, canvas, tipo_cor_var, tipo_preenchimento_var, tipo_figura_var):
-        self.canvas = canvas
-        self.figuras = []
-        self.figura_atual = None
-        self.tipo_cor_var = tipo_cor_var
-        self.tipo_preenchimento_var = tipo_preenchimento_var
-        self.tipo_figura_var = tipo_figura_var
-
-    def iniciar_figura(self, event):
-        # Pega a cor da borda e converte usando o SeletorCor
-        cor_b = SeletorCor.converter(self.tipo_cor_var.get())
-
-        # Se for transparente deixa vazio, senão converte a cor
-        if self.tipo_preenchimento_var.get() == 'Transparente':
-            cor_p = ""
-        else:
-            cor_p = SeletorCor.converter(self.tipo_preenchimento_var.get())
-
-        # Pega o tipo da forma selecionada
-        tipo = self.tipo_figura_var.get()
-
-        # Cria o objeto certo dependendo do que está selecionado no menu
-        if tipo == 'Linha':
-            self.figura_atual = FiguraLinha('linha', [event.x, event.y, event.x, event.y], cor_b, cor_p)
-        elif tipo == 'Retangulo':
-            self.figura_atual = FiguraRetangulo('retangulo', [event.x, event.y, event.x, event.y], cor_b, cor_p)
-        elif tipo == 'Rabisco':
-            self.figura_atual = FiguraRabisco('rabisco', [(event.x, event.y)], cor_b, cor_p)
-        elif tipo == 'Circulo':
-            self.figura_atual = FiguraCirculo('circulo', [event.x, event.y, 0], cor_b, cor_p)
-        else:
-            self.figura_atual = FiguraOval('oval', [event.x, event.y, 0, 0], cor_b, cor_p)
-
-
-    def atualizar_figura(self, event):
-            if self.figura_atual is not None:
-                self.figura_atual.atualizar(event.x, event.y)
-                self.redesenhar()
-
-
-
-"""     def incluir_figura(self, event):
-            if self.figura_atual is not None:
-                if not self.figura_atual.incompleta():
-                    self.figuras.append(self.figura_atual)
-
-                self.figura_atual = None
-                self.redesenhar() """
-
-
+class Armazem:
+    def __init__(self):
+        self.__figuras = []
+    
+    def incluir_figura(self, figura_atual):
+        self.__figuras.append(figura_atual)
+    def obter_figuras(self):
+        return self.__figuras
