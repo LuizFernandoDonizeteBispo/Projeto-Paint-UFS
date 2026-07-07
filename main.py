b92d9c96d5c78cefd3201077dc2e241563b36864
lucassa <lucassabarretosantos@gmail.com>
2026-07-07 20:35:01 -0300

    correçao de import nas classes e na expansao do canvas

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ from tkinter import Tk, Frame, Canvas, W
 from Model import *
 from View.menu import Menu
 from View.quadro import Quadro
-from View.desenho import Desenho
+from View.janelaPaint import JanelaPaint
+from Controller.Desenhar import Desenho
 from Controller.mouse import Mouse
-
 
 
 #******* MAIN *******#
 
 root = Tk()
+root.state('zoomed')
 menu = Menu(root)
 menu.montar()
 quadro = Quadro(root, menu.tipo_preenchimento_var, menu.tipo_cor_var, menu.tipo_figura_var)
 desenho = Desenho(quadro.canvas, menu.tipo_cor_var, menu.tipo_preenchimento_var, menu.tipo_figura_var)
-
+janela_paint = JanelaPaint(quadro.canvas)
+mouse = Mouse(quadro.canvas, desenho)
+mouse.mouse()
 root.mainloop()
